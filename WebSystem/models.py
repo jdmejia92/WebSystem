7fc3c17e1793a58abfb9e2fe4c3045f7f3055ff5
Juan Daniel Mejia Hoyos <jdmejia92@icloud.com>
2022-10-21 20:03:14 -0400

    Creacion de usuarios

--- WebSystem/models.py
+++ WebSystem/models.py
@@ -4,4 +4,4 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin):
     def __init__(self, id, name, email, password, is_admin=False):
-        
+        pass
--- WebSystem/routes.py
+++ WebSystem/routes.py
@@ -1,16 +1,21 @@
-from flask_login import LoginManager
+#from flask_login import LoginManager
 from WebSystem import app
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, url_for, redirect
 #from WebSystem.models import DataProcess
 
 route_db = app.config['ROUTE_BBDD']
-login_manager = LoginManager(app)
+#login_manager = LoginManager(app)
 #data_manager =
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    return render_template("base.html")
 
-@app.route("/api/v01/login")
-def machines():
-    return
+@app.route("/api/v01/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        print(request.form)
+        return "Access Granted"
+    else:
+        print(request.form)
+        return redirect(url_for("start"))
